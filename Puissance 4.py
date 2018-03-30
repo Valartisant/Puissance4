b3446e14ae0232c3374148d2da15d76c1274753f
Valartisant <35802732+Valartisant@users.noreply.github.com>
2018-03-30 20:09:37 +0200

    Update Puissance 4.py

--- Puissance 4.py
+++ Puissance 4.py
@@ -22,12 +22,15 @@ def Affiche(M):
 # Tour du joueur :
 #Teste si la lettre entrée par le joueur est dans la liste ref, et si c'est le cas renvoie 0 pour A, 1 pour B, ect
 
-def joueur() :
-    reponse = input("Où voulez-vous jouer ? ")
+def joueur(reponse) :
+    #reponse = input("Où voulez-vous jouer ? ")
     ref = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-    for k in range(len(ref)):
-        if reponse == ref[k]:
-            return k
+    
+    if reponse in ref :
+        return ref.index(reponse)
+    else:
+        print('Error')
+        joueur()
 # Rappel de la fonction si erreur :
 # print('Veuillez entrer la lettre correspondant la colonne choisie')
 # joueur()
@@ -37,15 +40,18 @@ def joueur() :
 #Place le pion du joueur dans la première case innocupé de la colonne numérotée 'nbre'
 def plaçagedupion(nbre):
     n = 1
-    for k in range(1, 6):
-        if plateau[nbre+2][k+1] == '.':
+    for k in range(5):
+        if plateau[nbre+1][k+2] == '.':
             n += 1
     plateau[n][nbre+2] = "x"
-
 # Pour l'animation : il faudrait modifier 'en direct' le plateau qui serait tout le temps affiché
 
 
 Affiche(plateau)
-plaçagedupion(joueur())
+#plaçagedupion(joueur())
+ref = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+for l in ref :
+    print(l)
+    plaçagedupion(joueur(l))
 Affiche(plateau)
 
