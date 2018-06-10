3af58dd2181be1bd8be1384f954faee7b9719605
Valartisant <35802732+Valartisant@users.noreply.github.com>
2018-06-10 15:00:42 +0200

    Correction utilisation Alpha-Beta et affichage coups

--- puissance4.py
+++ puissance4.py
@@ -13,6 +13,7 @@ PION_JOUEUR_1 = "X"
 PION_JOUEUR_2 = "O"
 
 UTILISATION_APLHA_BETA = True
+AFFICHAGE_NBRE_COUPS_SIMULES = False
 
 
 class Grille(object):
@@ -652,8 +653,8 @@ class PartieConsole(object):
         if nbre_joueurs_humains == 0:
             profondeur = self._demander_profondeur_ia()
             if profondeur != -1:
-                self._joueur1 = IAMinMax(self._jeu, 'MinMax1', PION_JOUEUR_1, PION_JOUEUR_2, profondeur)
-                self._joueur2 = IAMinMax(self._jeu, 'MinMax2', PION_JOUEUR_2, PION_JOUEUR_1, profondeur)
+                self._joueur1 = IAMinMax(self._jeu, 'MinMax1', PION_JOUEUR_1, PION_JOUEUR_2, profondeur, UTILISATION_APLHA_BETA)
+                self._joueur2 = IAMinMax(self._jeu, 'MinMax2', PION_JOUEUR_2, PION_JOUEUR_1, profondeur, UTILISATION_APLHA_BETA)
             else:
                 self._joueur1 = IaAleatoire(self._jeu, 'Random1', PION_JOUEUR_1)
                 self._joueur2 = IaAleatoire(self._jeu, 'Random2', PION_JOUEUR_2)
@@ -666,7 +667,7 @@ class PartieConsole(object):
             profondeur = self._demander_profondeur_ia()
             self._joueur1 = JoueurHumain(self._jeu, nom, PION_JOUEUR_1)
             if profondeur != -1:
-                self._joueur2 = IAMinMax(self._jeu, 'MinMax', PION_JOUEUR_2, PION_JOUEUR_1, profondeur)
+                self._joueur2 = IAMinMax(self._jeu, 'MinMax', PION_JOUEUR_2, PION_JOUEUR_1, profondeur, UTILISATION_APLHA_BETA)
             else:
                 self._joueur2 = IaAleatoire(self._jeu, 'Random', PION_JOUEUR_2)
 
@@ -817,6 +818,8 @@ class IAMinMax(object):
             # (si tous les coups possibles ont été testés la boucle s'arrête)
 
         # Parmi tous les coups possibles de l'IA on retourne le meilleur
+        if AFFICHAGE_NBRE_COUPS_SIMULES:
+            print(self.nbre_coups_simules)
         self.nbre_coups_simules = 0
         return coup_retenu.colonne
 
@@ -858,6 +861,8 @@ class IAMinMax(object):
             # (si tous les coups possibles ont été testés la boucle s'arrête)
 
         # Parmi tous les coups possibles de l'IA on retourne le meilleur
+        if AFFICHAGE_NBRE_COUPS_SIMULES:
+            print(self.nbre_coups_simules)
         self.nbre_coups_simules = 0
         return coup_retenu.colonne
 
