93d31b2468aa852889140ac9707c4a45f4b7e6ef
thomas40510 <thomasprevost85@gmail.com>
2018-06-11 19:47:12 +0200

    began commenting and cleaning

--- p4online.py
+++ p4online.py
@@ -6,7 +6,6 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-#status = 'h'
 
 nameList = ["VAL", "THMS", "CHRISTOPHE", "LUDOVIC"] #Easter-Egg - si le nom entré par le joueur figure dans cette liste, il est remplacé par un nom de altName
 altName = ["Valartisant", "Lalicorne", "Mr. Durant", "Mr. Rion"]
@@ -15,28 +14,24 @@ sName = "sName"
 
 
 def init():
+    """
+    Connexion à la racine de la BDD et authentification auprès du serveur
+    """
     oppName = "none"
     selfName = "none"
-    # Fetch the service account key JSON file contents
+    # On récupère les informations de connexion
     path = os.path.join(sys.path[0], 'service-account-credentials.json')
 
     cred = credentials.Certificate('serviceAccountCredentials.json')
-    # Initialize the app with a None auth variable, limiting the server's access
+    # On initialise la connexion au serveur
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://puissance-4-cyka.firebaseio.com/',
         'databaseAuthVariableOverride': None
     })
 
     global ref
-    ref = db.reference("/1v1-online")
+    ref = db.reference("/1v1-online") #On prend la partie "online" de la BDD
 
-    # The app only has access to public data as defined in the Security Rules
-
-'''
-ref = db.reference('/test')
-str = ref.get()
-print(str)
-'''
 
 def screenClear():
     """
@@ -44,8 +39,11 @@ def screenClear():
     """
     os.system('cls' if os.name == 'nt' else 'clear')
 
-#ANY - asks if player wants to create or join, and sets status
+#TOUS - asks if player wants to create or join, and sets status
 def setStatus():
+    """
+    On demande au joueur s'il veut rejoindre ou créer une partie, et on lui affecte un statut
+    """
     print('Que Voulez-vous faire ?\n')
     global status
     i = input(" [1] Créer une partie\n [2] Rejoindre une partie\n [exit] Retourner au menu\n\n >>")
@@ -54,16 +52,18 @@ def setStatus():
         setStatus()
     else:
         if (i=='1'):
-            status = 'h'
+            status = 'h' #Si le joueur crée une partie, il devient hôte
         elif (i=='2') :
-            status ='g'
+            status ='g' #Sinon il devient invité
         else :
-            return subprocess.call("launch.bat", shell=True)
+            return subprocess.call("launch.bat", shell=True) #Retour au menu si le joueur veut quitter
         varSet(status)
         return True
 
-#ANY - Set vars based on status
 def varSet(status):
+    """
+    On affecte le nom des références à la BDD en fonction du statut du joueur
+    """
     global o_last, o_play, o_replay, o_name
     global s_last, s_play, s_replay, s_name
     if status == 'h':
@@ -86,88 +86,96 @@ def varSet(status):
         s_name='g_name'
     return True
 
-#HOST - create lobby
 def newlobby():
+    """
+    (Hôte seulement)
+    On crée un salon, on s'assure qu'il est bien remis à zéro et on le met à jour
+    """
     if (status=='h'):
-        lobbylist = ref.get()
-        print(lobbylist)
+        lobbylist = ref.get() #Récupère la liste des salons sur la BDD
         for lobby in lobbylist:
             current = ref.child(lobby)
             global myref
             myref = ref.child(lobby)
-            if(current.child('gameOn').get()=="False"):
+            if(current.child('gameOn').get()=="False"): #Vérifie si le salon n'est pas déjà occupé
                 current.update({"gameOn" : "True"})
                 myref.update({"playercount" : 1})
-                resetlobby()
-                getName()
-                wait()
-
+                resetlobby() #On remet le salon à zéro
+                getName() #On demande au joueur son nom
+                wait() #On attend un adversaire
                 return True
-        print('No available lobby. Try to join one, or try again later.')
-        input('press any key to continue...')
-        subprocess.call("welcome.py", shell=True)
+        print('Malheureusement, il n\'y a pas de salon disponible... Merci de réessayer plus tard.)
+        input('Presser une touche pour continuer...')
+        subprocess.call("launch.bat", shell=True) #On relance le programme
         return False
     else:
-        joinlobby()
+        joinlobby() #Si le joueur a le statut d'invité, on le redirige vers la fonction correspondante
 
 def wait():
+    """
+    (Hôte seulement)
+    Écran d'attente
+    """
     screenClear()
-    print("Vous avez rejoint le lobby n°"+myref.child("nbr").get())
-    #print("Vous avez rejoint le lobby n°"+str(myref)[-2]+"\n")
+    print("Vous avez rejoint le lobby n°"+myref.child("nbr").get()) #On affiche le salon qui vient d'être créé
     i = 0
-    while (myref.child('playercount').get()!=2):
-        if(i!=30):
+    while (myref.child('playercount').get()!=2): #Tant qu'un autre joueur n'a pas rejoint, on affiche un message d'attente
+        if(i!=30): #On met en place un timer
             time.sleep(0.5)
-            #sys.stdout.write("\r" +'waiting for opponent to join...'+ (i/3)*'.')
-            sys.stdout.write("\r" + 'waiting for opponent to join.' + (i % 3) * '.' + (16 - (i % 4)) * ' ')
+            sys.stdout.write("\r" + 'En attente d\'un adversaire.' + (i % 3) * '.' + (16 - (i % 4)) * ' ')
             sys.stdout.flush()
             i+=1
         else :
-            print('no opponent seems to come...')
+            #Si le temps d'attente devient long, on demande au joueur s'il veut continuer d'attendre ou pas
+            print('\n Aucun adversaire ne semble arriver...')
             n = ""
             while(n not in ('o','n')):
                 n = input('Voulez-vous continuer d\'attendre ? o/n ')
             if n == 'o':
                 wait()
             else :
-                fullReset()
-                subprocess.call("launch.bat", shell=True)
+                fullReset() #On remet le salon entièrement à zéro
+                subprocess.call("launch.bat", shell=True) #On relance le jeu
                 exit()
 
-
-
-#GUEST - show available lobbies to join
 def showlobbies():
+    """
+    (Invité seulement)
+    On affiche les salons ouverts et disponibles
+    """
     global currentGames
     currentGames = []
     gameList=[]
     lobbylist = ref.get()
-    for lobby in lobbylist:
+    for lobby in lobbylist: #Pour chaque salon, on regarde s'il est activé, et s'il n'y a qu'un joueur
         if (ref.child(lobby).child('gameOn').get()=='True' and ref.child(lobby).child('playercount').get()!=2):
-            host = ref.child(lobby).child("h_name").get()
+            host = ref.child(lobby).child("h_name").get() #On récupère le nom de l'hôte
             currentGames.append(lobby)
             gameList.append(lobby + ' ('+host+')')
-    if (len(currentGames) == 0):
-        print('no games. You can create a game, instead.')
-        input('press any key to go back...')
+    if (len(currentGames) == 0): #S'il n'y a aucun salon disponible
+        print('Aucun salon n\'est disponible. Vous pouvez essayer d\'en créer un, à la place.')
+        input('Presser une touche pour continuer...')
         subprocess.call("launch.bat", shell=True)
         status=""
         exit()
         return False
     print('')
-    for s in gameList:
+    for s in gameList: #On affiche les salons disponibles
         print("- "+s)
     print('')
 
-#GUEST - join a lobby
 def joinlobby():
-    screenClear()
-    print("Open lobbies :")
+    """
+    (Invité seulement)
+    Le joueur choisit un salon, et le programme y établit la connexion
+    """
+    screenClear() #On efface la console
+    print("Salons disponibles :")
     showlobbies()
-    i = input("which one you join ? Enter number (exit to quit): ")
-    if i.upper()=="EXIT":
-        subprocess.call("launch.bat", shell=True)
-        status=""
+    i = input("Lequel voulez-vous rejoindre ? (Écrivez le numéro correspondant, ou \'exit\' pour quitter.) \n\n >>")
+    if i.upper()=="EXIT": #Si le joueur veut quitter
+        subprocess.call("launch.bat", shell=True) #on retourne au menu
+        status="" #On réinitialise le statut du joueur
         exit()
         return False
     elif (i not in ('1','2','3')):
@@ -176,33 +184,29 @@ def joinlobby():
         joinlobby()
     else :
         current = []
-        for s in currentGames:
+        for s in currentGames: #On crée une liste contenant les numéros des salons disponibles
             current.append(s[-1])
         if i not in current :
-            print("Ce lobby n'est pas disponible. Il est peut-être complet ou vide...")
+            print("Ce salon n'est pas disponible. Il est peut-être complet ou vide...")
             time.sleep(2)
             joinlobby()
 
-        lobby = 'lobby'+i
+        lobby = 'lobby'+i #On fabrique une référence à la BDD à partir de l'entrée utilisateur
         global myref
-        myref = ref.child(lobby)
-        getName()
-        myref.update ({'playercount' : 2})
+        myref = ref.child(lobby) #On établit la connexion au salon choisi
+        getName() #On récupère le nom du joueur
+        myref.update ({'playercount' : 2}) #On met à jour la BDD
         global savedLp
-        savedLp = myref.child('h_last').get()
+        savedLp = myref.child('h_last').get() #On initialise la variable contenant le timecode du dernier coup de l'hôte
         return True
     return False
 
-#HOST - set default values
 def resetlobby():
+    """
+    (Hôte seulement)
+    On remet la BDD à zéro en début de partie
+    """
     if (status=='h'):
-        #if (input('Do you want to play first ? Y/N')=='Y'):
-        '''
-        if True:
-            whoplays = 1
-        else:
-            whoplays = 2
-        '''
         myref.update({
         "g_last" : "none",
         "g_play" : "0",
@@ -211,72 +215,80 @@ def resetlobby():
         "h_play" : "0",
         "h_replay" : "0",
         "replayOn" : "False",
-        #"whoplays" : whoplays,
         })
         global savedLp
-        savedLp = myref.child('g_last').get()
+        savedLp = myref.child('g_last').get() #On initialise la variable contenant le timecode du dernier coup de l'invité
         return True
     return False
 
 def fullReset():
+    """
+    (Hôte seulement)
+    Remise à zéro de la BDD en fin de partie
+    """
     if (status=='h'):
         myref.update({
-        #"g_last" : "none",
-        #"g_play" : "0",
         "g_replay" : "0",
         "g_name" : "gname",
-        #"h_last" : "none",
         "h_name" : "hname",
         "gameOn" : "False",
-        #"h_play" : "0",
         "h_replay" : "0",
         "replayOn" : "False",
         "playercount" : 0
         })
 
-#ANY - asks for player's name
 def getName():
+    """
+    On demande son nom à l'utilisateur
+    """
     name = input("Quel est votre nom ? \n\n >>")
-    if name.upper() in nameList:
+    if name.upper() in nameList: #Si le nom rentré est dans la liste de l'Easter Egg, on le remplace en conséquence
         name = altName[nameList.index(name.upper())]
-    i = input("Vous vous appelez " + name + " c'est bien ça ? [o/n] \n\n>>")
+    i = input("Vous vous appelez " + name + " c'est bien ça ? [o/n] \n\n>>") #Le joueur valide son nom
     while (i not in ("o","n")):
         i = input("Je n'ai pas bien compris... \n\n>>")
     if i == "o":
         print("Bien reçu !")
         global sName
-        sName = name
+        sName = name #On affecte à la variable globale la valeur de la variable locale 'name'
         pushName()
     else:
-        getName()
+        getName() #si l'utilisateur répond 'non', on lui redemande son nom
 
-#ANY - pushes player's name to server
 def pushName():
+    """
+    On envoie le nom rentré par le joueur à la BDD
+    """
     myref.update({s_name: sName})
 
-#ANY - Fetch Opponent's name
 def oppName():
+    """
+    On récupère le nom de l'adversaire
+    """
     return myref.child(o_name).get()
 
-#ANY - fetches opponent's last action from db
 def hisTurn():
+    """
+    On récupère le dernier coup de l'adversaire
+    """
     global savedLp
     i = 0
-    while(myref.child(o_last).get() == savedLp):
+    while(myref.child(o_last).get() == savedLp): #On affiche un message d'attente tant que l'adversaire n'a pas joué
          time.sleep(0.5)
          #sys.stdout.write("\r" +'waiting for opponent to play...'+ i*'.')
-         sys.stdout.write("\r" + 'waiting for opponent to play.' + (i % 3) * '.' + (16 - (i % 4)) * ' ')
+         sys.stdout.write("\r" + 'L\'adversaire joue.' + (i % 3) * '.' + (16 - (i % 4)) * ' ')
          sys.stdout.flush()
          i+=1
-    n = myref.child(o_play).get()
-    savedLp = myref.child(o_last).get()
-    print(n)
-    return n
+    n = myref.child(o_play).get() #On récupère le coup de l'adversaire
+    savedLp = myref.child(o_last).get() #On enregiste le timecode correspondant au coup actuel
+    return n #On renvoie le coup de l'adversaire
 
-#ANY - Handles action of player and sends it to server
 def myturn(n):
-    myref.update({s_play : n})
-    myref.update({s_last : time.time()})
+    """
+    Envoie le coup de l'utilisateur à la BDD
+    """
+    myref.update({s_play : n}) #On enregistre le coup
+    myref.update({s_last : time.time()}) #On enregistre le timecode
     return True
 
 #ANY - Says if player wants to replay
